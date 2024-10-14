1f043614b942dbc42092b3164c29977ba60bef21
ShivaniSeth5 <shivaniseth757@gmail.com>
2024-10-14 20:44:34 +0000

    Reference type

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -13,7 +13,18 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 
 console.log(id == anotherId);
+// const bigNumber = 34565868690n
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = { 
+    name: "shivani",
+    age: 30,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
